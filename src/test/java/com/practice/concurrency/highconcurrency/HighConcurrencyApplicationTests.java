c48bfbdaf9d386ad02138cf4696f43b22deeb944
keweizhou <1514991931@qq.com>
2020-05-04 10:15:42 +0800

    init project

---
+++ src/test/java/com/practice/concurrency/highconcurrency/HighConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.concurrency.highconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
