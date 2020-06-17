4d43474a67a0d5d547aa1057e0aabb35843e2478
keweizhou <1514991931@qq.com>
2020-06-18 00:51:54 +0800

    add annotation

---
+++ src/main/java/com/practice/concurrency/highconcurrency/annoation/NotThreadSafe.java
@@ -0,0 +1,19 @@
+package com.practice.concurrency.highconcurrency.annoation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Description
+ * 用来标注线程不安全的类和写法
+ * Date 2020/6/18 0:45
+ * Created by kwz
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
