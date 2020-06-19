b20e080b0f755af4f633096d6dd9f3aba19f092c
keweizhou <1514991931@qq.com>
2020-06-20 00:52:01 +0800

    add atomicboolean

---
+++ src/main/java/com/practice/concurrency/highconcurrency/count/atomic/AtomicBooleanTest.java
@@ -0,0 +1,60 @@
+package com.practice.concurrency.highconcurrency.count.atomic;
+
+import com.practice.concurrency.highconcurrency.annoation.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Description
+ * Date 2020/6/18 22:45
+ * Created by kwz
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicBooleanTest {
+
+    private static AtomicBoolean isHappened = new AtomicBoolean(false);
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(
+                    () -> {
+                        try {
+                            semaphore.acquire();
+                            test();
+                            semaphore.release();
+                        } catch (InterruptedException e) {
+                            log.error("exception", e);
+                        }
+                        countDownLatch.countDown();
+                    }
+            );
+        }
+        countDownLatch.await();
+        //线程池用完之后关闭它
+        executorService.shutdownNow();
+        log.info("isHappened:{}", isHappened.get());
+    }
+
+    /**
+     * 只执行一次，执行完了就不执行了
+     */
+    private static void test() {
+        if (isHappened.compareAndSet(false, true)) {
+            log.info("execute");
+        }
+    }
+}
