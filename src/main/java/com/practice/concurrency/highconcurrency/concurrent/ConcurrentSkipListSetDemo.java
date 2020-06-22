6f962cbcc1b3c09b0a82f862613405ea4e98f1f7
keweizhou <1514991931@qq.com>
2020-06-22 22:06:46 +0800

    CopyOnWriteArrayList,CopyOnWriteArraySet,ConcurrentSkipListSet

---
+++ src/main/java/com/practice/concurrency/highconcurrency/concurrent/ConcurrentSkipListSetDemo.java
@@ -0,0 +1,53 @@
+package com.practice.concurrency.highconcurrency.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * Description
+ * ArrayList、HashSet、HashMap都不是线程安全的
+ * Date 2020/6/21 21:42
+ * Created by kwz
+ */
+@Slf4j
+public class ConcurrentSkipListSetDemo {
+
+    private static Set<Integer> list = new ConcurrentSkipListSet<>();
+
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 50;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(
+                    () -> {
+                        try {
+                            semaphore.acquire();
+                            update(count);
+                            semaphore.release();
+                        } catch (InterruptedException e) {
+                            log.error("exception", e);
+                        }
+                        countDownLatch.countDown();
+                    }
+            );
+        }
+        countDownLatch.await();
+        //线程池用完之后关闭它
+        executorService.shutdownNow();
+        log.info("size:{}", list.size());
+    }
+
+    private static void update(int i) {
+        list.add(i);
+    }
+}
---
+++ src/main/java/com/practice/concurrency/highconcurrency/synContainer/VectorDemo3.java
@@ -0,0 +1,57 @@
+package com.practice.concurrency.highconcurrency.synContainer;
+
+import com.practice.concurrency.highconcurrency.annoation.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ * Description
+ * ArrayList、HashSet、HashMap都不是线程安全的
+ * Date 2020/6/21 21:42
+ * Created by kwz
+ */
+@Slf4j
+@NotThreadSafe
+public class VectorDemo3 {
+
+    //foreach
+    //Exception in thread "main" java.util.ConcurrentModificationException
+    private static void test1(Vector<Integer> vector) {
+        for (Integer curNum : vector) {
+            if (curNum.equals(3)) {
+                vector.remove(curNum);
+            }
+        }
+    }
+
+    //iterator
+    //Exception in thread "main" java.util.ConcurrentModificationException
+    private static void test2(Vector<Integer> vector) {
+        Iterator<Integer> iterator = vector.iterator();
+        while (iterator.hasNext()) {
+            Integer curNum = iterator.next();
+            if (curNum.equals(3)) {
+                vector.remove(curNum);
+            }
+        }
+    }
+
+    //for
+    private static void test3(Vector<Integer> vector) {
+        for (int i=0;i<vector.size();i++){
+            if (vector.get(i).equals(3)){
+                vector.remove(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        test3(vector);
+    }
+}
