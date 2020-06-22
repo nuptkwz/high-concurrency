7c8ac895adae7db542b4cf4d58cbb97088c544e6
keweizhou <1514991931@qq.com>
2020-06-23 00:13:29 +0800

    annotation

--- src/main/java/com/practice/concurrency/highconcurrency/juc/CountDownLatchExample1.java
+++ src/main/java/com/practice/concurrency/highconcurrency/juc/CountDownLatchExample1.java
@@ -37,6 +37,7 @@ public class CountDownLatchExample1 {
                     }
             );
         }
+        //调用await这个方法的类一直处于阻塞状态，所有的线程调用完之后再打印finish
         countDownLatch.await();
         log.info("finish");
         executorService.shutdown();
