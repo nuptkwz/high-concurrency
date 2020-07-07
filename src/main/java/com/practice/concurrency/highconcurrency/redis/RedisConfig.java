48c794c2e917130ce511b4d00d84db4ede3cd645
keweizhou <1514991931@qq.com>
2020-07-07 08:22:00 +0800

    redis config

---
+++ src/main/java/com/practice/concurrency/highconcurrency/redis/RedisConfig.java
@@ -0,0 +1,16 @@
+package com.practice.concurrency.highconcurrency.redis;
+
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Description
+ * Date 2020/7/6 23:40
+ * Created by kwz
+ */
+
+
+@Configuration
+public class RedisConfig {
+
+}
