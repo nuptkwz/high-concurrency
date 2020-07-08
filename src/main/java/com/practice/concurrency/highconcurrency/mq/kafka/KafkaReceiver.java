e7d7ab74c6f8378238f8ca729280d0c896b3030a
keweizhou <1514991931@qq.com>
2020-07-08 21:45:35 +0800

    add kafka config

--- src/main/java/com/practice/concurrency/highconcurrency/mq/kafka/KafkaReceiver.java
+++ src/main/java/com/practice/concurrency/highconcurrency/mq/kafka/KafkaReceiver.java
@@ -1,9 +1,21 @@
 package com.practice.concurrency.highconcurrency.mq.kafka;
 
+import lombok.extern.slf4j.Slf4j;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.stereotype.Component;
+
 /**
  * Description
  * Date 2020/7/8 21:28
  * Created by kwz
  */
+@Component
+@Slf4j
 public class KafkaReceiver {
+
+
+    @KafkaListener(topics = {TopicConstants.TEST})
+    public void receive(ConsumerRecord<?, ?> record) {
+        log.info("record:{}", record);
+    }
 }
---
+++ src/main/java/com/practice/concurrency/highconcurrency/mq/rabbitmq/QueueConstants.java
@@ -0,0 +1,12 @@
+package com.practice.concurrency.highconcurrency.mq.rabbitmq;
+
+/**
+ * Description
+ * Date 2020/7/8 21:44
+ * Created by kwz
+ */
+public class QueueConstants {
+    String TEST = "test";
+
+    String MESSAGE = "message";
+}
