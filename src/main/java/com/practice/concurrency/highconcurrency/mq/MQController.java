50a30abb4a211b878dc2abc47480bb618ec8eb29
keweizhou <1514991931@qq.com>
2020-07-11 00:12:03 +0800

    add kafka pom

--- src/main/java/com/practice/concurrency/highconcurrency/mq/MQController.java
+++ src/main/java/com/practice/concurrency/highconcurrency/mq/MQController.java
@@ -1,8 +1,11 @@
 package com.practice.concurrency.highconcurrency.mq;
 
+import com.practice.concurrency.highconcurrency.mq.kafka.KafkaSender;
 import com.practice.concurrency.highconcurrency.mq.rabbitmq.RabbitMQClient;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -19,4 +22,18 @@ public class MQController {
     @Resource
     private RabbitMQClient rabbitMQClient;
 
+    @Resource
+    private KafkaSender kafkaSender;
+
+    @PostMapping("/rabbit-send")
+    public String send(@RequestParam("message") String message) {
+        rabbitMQClient.send(message);
+        return "success";
+    }
+
+    @PostMapping("/kafka-send")
+    public String kafkaSend(@RequestParam("message") String message) {
+        kafkaSender.send(message);
+        return "success";
+    }
 }
--- src/main/java/com/practice/concurrency/highconcurrency/mq/kafka/KafkaSender.java
+++ src/main/java/com/practice/concurrency/highconcurrency/mq/kafka/KafkaSender.java
@@ -1,9 +1,12 @@
 package com.practice.concurrency.highconcurrency.mq.kafka;
 
+import com.alibaba.fastjson.JSON;
 import com.practice.concurrency.highconcurrency.mq.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Date;
 
 /**
@@ -15,15 +18,16 @@ import java.util.Date;
 @Slf4j
 public class KafkaSender {
 
-//    private Kafka
+    @Resource
+    private KafkaTemplate<String, String> kafkaTemplate;
 
-    private void send(String msg) {
+    public void send(String msg) {
         Message message = Message.builder()
                 .id(System.currentTimeMillis())
                 .msg(msg)
                 .sendTime(new Date())
                 .build();
         log.info("send message:{}", message);
-
+        kafkaTemplate.send(TopicConstants.TEST, JSON.toJSONString(message));
     }
 }
