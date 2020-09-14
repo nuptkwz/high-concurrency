6903e7e6247f9e0018a5ee80605274e5c0173665
keweizhou <1514991931@qq.com>
2020-09-14 22:31:56 +0800

    equals and hashcode

--- src/main/java/com/practice/concurrency/highconcurrency/User.java
+++ src/main/java/com/practice/concurrency/highconcurrency/User.java
@@ -2,6 +2,10 @@ package com.practice.concurrency.highconcurrency;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Description
  * Date 2020/6/21 17:35
@@ -11,4 +15,30 @@ import lombok.Data;
 public class User {
     private String userId;
     private String userName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getUserId(), user.getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserId());
+    }
+
+    public static void main(String[] args) {
+
+        User user1 = new User();
+        user1.setUserId("2");
+        user1.setUserName("hah");
+
+        User user2 = new User();
+        user2.setUserId("1");
+        user2.setUserName("hahhh");
+
+        System.out.println(user1.equals(user2));
+    }
 }
