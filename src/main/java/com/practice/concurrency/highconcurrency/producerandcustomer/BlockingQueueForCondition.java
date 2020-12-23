e9bf2b33bb63ba9aefc8d5620bbef91c465d50ba
keweizhou <1514991931@qq.com>
2020-12-23 23:15:42 +0800

    利用Condition实现生产者消费者

--- src/main/java/com/practice/concurrency/highconcurrency/producerandcustomer/BlockingQueueForCondition.java
+++ src/main/java/com/practice/concurrency/highconcurrency/producerandcustomer/BlockingQueueForCondition.java
@@ -23,9 +23,42 @@ public class BlockingQueueForCondition {
     private Condition notEmptyCondition = lock.newCondition();
     private Condition notFullCondition = lock.newCondition();
 
-    public BlockingQueueForCondition(int size){
+    public BlockingQueueForCondition(int size) {
         this.max = size;
         queue = new LinkedList();
+    }
+
+    public void put(Object object) throws InterruptedException {
+        //多线程场景需要一定的同步措施来保证线程安全，因此需要加锁操作
+        lock.lock();
+        try {
+            //检测队列是否已经满了
+            while (queue.size() == max) {
+                //此时队列已经满了，阻塞生产者线程并且释放lock锁
+                notFullCondition.await();
+            }
+            //如果没有满则往队列里面存放数据，并且调用notEmptyCondition.signalAll()通知正在等待的消费者，
+            //并且唤醒等待的消费者
+            queue.add(object);
+            notEmptyCondition.signalAll();
+        } finally {
+            //最后释放锁,否则可能会产生无法释放锁的情况
+            lock.unlock();
+        }
+    }
+
+    public Object take() throws InterruptedException {
+
+        try {
+            while (queue.isEmpty()) {
+                notEmptyCondition.await();
+            }
+            Object item = queue.remove();
+            notFullCondition.signalAll();
+            return item;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public static void main(String[] args) {
