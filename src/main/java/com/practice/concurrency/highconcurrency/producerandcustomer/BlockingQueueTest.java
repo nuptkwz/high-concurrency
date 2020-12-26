75295b3a2f1e0c37c2e18e3f06c3900dadb1e6bc
keweizhou <1514991931@qq.com>
2020-12-26 12:15:20 +0800

    BlockingQueueFor Wait,notifyAll test

--- src/main/java/com/practice/concurrency/highconcurrency/producerandcustomer/BlockingQueueTest.java
+++ src/main/java/com/practice/concurrency/highconcurrency/producerandcustomer/BlockingQueueTest.java
@@ -1,5 +1,6 @@
 package com.practice.concurrency.highconcurrency.producerandcustomer;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -16,14 +17,47 @@ public class BlockingQueueTest {
     }
 }
 
+@Slf4j
 class Producer implements Runnable {
 
     private BlockingQueueForWaitNotify blockingQueue;
 
+    public Producer(BlockingQueueForWaitNotify blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    //    @SneakyThrows
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            blockingQueue.put(i);
+            try {
+                log.info("produce begin to put");
+                blockingQueue.put(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+@Slf4j
+class Consumer implements Runnable {
+
+    private BlockingQueueForWaitNotify blockingQueue;
+
+    public Consumer(BlockingQueueForWaitNotify blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                int item = blockingQueue.take();
+                log.info("consumer take:{}", item);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
