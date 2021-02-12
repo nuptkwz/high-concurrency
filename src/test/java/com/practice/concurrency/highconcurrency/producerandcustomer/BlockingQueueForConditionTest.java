bc0ebf976b80454c456791365feb338c41415e3e
keweizhou <1514991931@qq.com>
2021-02-12 11:05:59 +0800

    log

--- src/test/java/com/practice/concurrency/highconcurrency/producerandcustomer/BlockingQueueForConditionTest.java
+++ src/test/java/com/practice/concurrency/highconcurrency/producerandcustomer/BlockingQueueForConditionTest.java
@@ -1,9 +1,6 @@
 package com.practice.concurrency.highconcurrency.producerandcustomer;
 
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-
 /**
  * Description
  * Date 2020/12/26 14:44
